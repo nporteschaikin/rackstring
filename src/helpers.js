b9d29308d392251f2e2f5be71cdcac912d687b24
Noah Portes Chaikin <noah@porteschaikin.com>
2019-08-06 08:11:41 -0400

    Use `hasOwnProperty` to determine key existence
    This seems to be used everywhere else.

--- src/helpers.js
+++ src/helpers.js
@@ -2,3 +2,4 @@ export const isPlainObject = (obj) =>
   typeof obj === "object" && obj.constructor === Object
 
 export const isArray = (obj) => Array.isArray(obj)
+export const objectHas = (obj, key) => obj.hasOwnProperty(key)
