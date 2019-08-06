9285048fe524611982bb7e14dec4b1cf76b62620
Noah Portes Chaikin <noah@porteschaikin.com>
2019-08-06 09:23:58 -0400

    Improve tests

--- test/parse.test.js
+++ test/parse.test.js
@@ -21,11 +21,22 @@ test("parses array of objects", () => {
 })
 
 test("parses array of objects with arrays", () => {
-  expect(() =>
-    parse("a[][x]=b&a[][y]=c&a[][z][]=foo&a[][x]=d&a[][y]=e&a[][z][]=bar", {
-      depth: 1,
-    })
-  ).toThrow(RangeError)
+  expect(
+    parse("a[][x]=b&a[][y]=c&a[][z][]=foo&a[][x]=d&a[][y]=e&a[][z][]=bar")
+  ).toEqual({
+    a: [{ x: "b", y: "c", z: ["foo"] }, { x: "d", y: "e", z: ["bar"] }],
+  })
+})
+
+test("parses array of objects with objects", () => {
+  expect(
+    parse("a[][x]=b&a[][y]=c&a[][z][foo]=foo&a[][x]=d&a[][y]=e&a[][z][foo]=bar")
+  ).toEqual({
+    a: [
+      { x: "b", y: "c", z: { foo: "foo" } },
+      { x: "d", y: "e", z: { foo: "bar" } },
+    ],
+  })
 })
 
 test("supports depth option", () => {
